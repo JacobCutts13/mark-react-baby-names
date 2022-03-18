c4aa6b4c936eb9ae81cd49b44c5ef1b1628182b6
Jc <Jc@email.com>
2022-03-18 10:35:24 +0000

    formatting

--- src/components/name.tsx
+++ src/components/name.tsx
@@ -1,14 +1,9 @@
 export interface BabyNameProps {
-    id: number;
-    name: string;
-    sex: string;
+  id: number;
+  name: string;
+  sex: string;
 }
 
 export default function BabyName(props: BabyNameProps): JSX.Element {
-    return(
-        <div className={"name" + props.sex}>
-            {props.name}
-        </div>
-    )
-
+  return <div className={"name" + props.sex}>{props.name}</div>;
 }
--- src/utils/alphabetSorter.ts
+++ src/utils/alphabetSorter.ts
@@ -1,3 +1,3 @@
-export default function alphabetSorter(nameA: string, nameB: string) :number{
-    return nameA.localeCompare(nameB)
+export default function alphabetSorter(nameA: string, nameB: string): number {
+  return nameA.localeCompare(nameB);
 }
